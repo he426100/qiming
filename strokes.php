d74f951b3d698d4bcf24259cdef1a8c8e9d1193e
弄月 <1090195622@qq.com>
2019-12-04 16:59:05 +0800

    修正其他文件对db的使用

--- strokes.php
+++ strokes.php
@@ -1,37 +1,9 @@
 <?php
 date_default_timezone_set('Asia/Shanghai');
 require __DIR__ . '/vendor/autoload.php';
+require 'db.php';
 
-use think\facade\Db;
-
-// 数据库配置信息设置（全局有效）
-Db::setConfig([
-    // 默认数据连接标识
-    'default'     => 'mysql',
-    // 数据库连接信息
-    'connections' => [
-        'mysql' => [
-            // 数据库类型
-            'type'     => 'mysql',
-            // 主机地址
-            'hostname' => '127.0.0.1',
-            // 用户名
-            'username' => 'root',
-            // 密码
-            'password' => 'root',
-            // 数据库名
-            'database' => 'myphp',
-            // 数据库编码默认采用utf8
-            'charset'  => 'utf8',
-            // 数据库表前缀
-            'prefix'   => 'youyin_',
-            // 数据库调试模式
-            'debug'    => true,
-        ],
-    ],
-]);
-
-$lists = Db::name('names')->where('name', 'like', '何%')->select(); //->where('buyiju_dafen', 0)
+$lists = Db::name('names')->where('name', 'like', '何%')->select();
 foreach ($lists as $vo) {
     $second_word = mb_substr($vo['name'], 1, 1);
     $third_word = mb_substr($vo['name'], 2, 1);
