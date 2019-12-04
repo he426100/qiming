6fa11327099e2b39efd7e7785532df9953a69748
弄月 <1090195622@qq.com>
2019-12-04 16:51:43 +0800

    修正对think-orm的使用

--- strokes.php
+++ strokes.php
@@ -2,32 +2,33 @@
 date_default_timezone_set('Asia/Shanghai');
 require __DIR__ . '/vendor/autoload.php';
 
-use think\Db;
+use think\facade\Db;
 
+// 数据库配置信息设置（全局有效）
 Db::setConfig([
-    // 数据库类型
-    'type'           => 'mysql',
-    // 服务器地址
-    'hostname'       => '127.0.0.1',
-    // 数据库名
-    'database'       => 'myphp',
-    // 用户名
-    'username'       => 'root',
-    // 密码
-    'password'       => 'root',
-    // 端口
-    'hostport'       => '',
-    // 连接dsn
-    'dsn'            => '',
-    // 数据库连接参数
-    'params'         => [],
-    // 数据库编码默认采用utf8
-    'charset'        => 'utf8',
-    // 数据库表前缀
-    'prefix'         => 'youyin_',
-    // 数据库调试模式
-    'debug'          => true,
-    // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+    // 默认数据连接标识
+    'default'     => 'mysql',
+    // 数据库连接信息
+    'connections' => [
+        'mysql' => [
+            // 数据库类型
+            'type'     => 'mysql',
+            // 主机地址
+            'hostname' => '127.0.0.1',
+            // 用户名
+            'username' => 'root',
+            // 密码
+            'password'       => 'root',
+            // 数据库名
+            'database' => 'myphp',
+            // 数据库编码默认采用utf8
+            'charset'  => 'utf8',
+            // 数据库表前缀
+            'prefix'   => 'youyin_',
+            // 数据库调试模式
+            'debug'    => true,
+        ],
+    ],
 ]);
 
 $lists = Db::name('names')->where('name', 'like', '何%')->select();//->where('buyiju_dafen', 0)
